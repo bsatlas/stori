1b8b5fe41dfeefb696c159bf1180d019431f2162
Atlas Kerr <atlaskerr@gmail.com>
2018-12-24 22:36:00 -0600

    http: Make metrics handler func unexported.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- http/handlers.go
+++ http/handlers.go
@@ -1,3 +1,17 @@
+// Copyright © 2018 Atlas Kerr atlaskerr@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package http
 
 import (
@@ -14,7 +28,7 @@ func Handler(props *stori.HandlerProperties) http.Handler {
 	h := http.NewServeMux()
 
 	// Metrics handler
-	h.Handle("/metrics", MetricsHandler())
+	h.Handle("/metrics", metricsHandler())
 
 	// OCI handler
 	h.Handle("/v2", OCIHandler(props))
@@ -22,8 +36,8 @@ func Handler(props *stori.HandlerProperties) http.Handler {
 	return h
 }
 
-// MetricsHandler returns an http.Handler for a Prometheus metrics endpoint.
-func MetricsHandler() http.Handler {
+// metricsHandler returns an http.Handler for a Prometheus metrics endpoint.
+func metricsHandler() http.Handler {
 	h := httprouter.New()
 	h.Handler("GET", "/metrics", promhttp.Handler())
 	return h
