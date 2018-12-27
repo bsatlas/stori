123adfdfd9c7e4aec079d7227215785c6a1adb95
Atlas Kerr <atlaskerr@gmail.com>
2018-12-26 22:59:04 -0600

    plugin: Add scaffolding for backend plugins.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- plugin/backend.go
+++ plugin/backend.go
@@ -3,19 +3,32 @@ package plugin
 import (
 	"net/rpc"
 
+	"github.com/atlaskerr/stori/stori"
+
 	"github.com/hashicorp/go-plugin"
 )
 
 // BackendPlugin implements plugin.Plugin.
-type BackendPlugin struct{}
+type BackendPlugin struct {
+	Backend func() stori.Backend
+}
 
 // Server returns a backend that can be communicates via RPC.
-func (be *BackendPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
-	return new(interface{}), nil
+func (be *BackendPlugin) Server(broker *plugin.MuxBroker) (interface{}, error) {
+	return &BackendServer{
+		Broker:  broker,
+		Backend: be.Backend(),
+	}, nil
 }
 
 // Client returns client used to interact with the backend server.
 func (be *BackendPlugin) Client(
 	*plugin.MuxBroker, *rpc.Client) (interface{}, error) {
 	return new(interface{}), nil
+}
+
+// BackendServer is a net/rpc compatible structure for serving a backend.
+type BackendServer struct {
+	Broker  *plugin.MuxBroker
+	Backend stori.Backend
 }
