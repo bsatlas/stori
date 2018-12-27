a12acbfe6a3df6d07e543b211e5f07d2ace0ea87
Atlas Kerr <atlaskerr@gmail.com>
2018-12-27 15:47:59 -0600

    stori: Add logger to Registry struct.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- stori/registry.go
+++ stori/registry.go
@@ -1,18 +1,24 @@
 package stori
 
-import ()
+import (
+	"go.uber.org/zap"
+)
 
 var defaultAddr = "0.0.0.0:7548"
 
 // Registry defines parameters for running a container image registry.
 type Registry struct {
+	logger zap.Logger
 }
 
-// Config is used to parameterize a registry.
+// RegistryConfig is used to parameterize a registry.
 type RegistryConfig struct {
+	Logger zap.Logger
 }
 
-// New takes a Config and returns a fully initialized Registry.
-func NewRegistry(*RegistryConfig) (*Registry, error) {
-	return &Registry{}, nil
+// NewRegistry takes a RegistryConfig and returns a fully initialized Registry.
+func NewRegistry(reg *RegistryConfig) (*Registry, error) {
+	return &Registry{
+		logger: reg.Logger,
+	}, nil
 }
