461d87aaeaa0002fe3add731f00dce0f768a30b5
Atlas Kerr <atlaskerr@gmail.com>
2019-01-03 14:16:31 -0600

    schema: Refactor validators into JSONLoaders.
    The original plan was to wrap up the gojsonschema library into easier to
    use domain-specific validators. It turns out that by hiding the
    complexity, working with validation errors becomes a huge pain.
    
    Schema libraries have been refactored to just provide
    a `gojsonschema.JSONloader` that can be used to validate against
    a specific schema.
    
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

---
+++ schema/oci/loader.go
@@ -0,0 +1,50 @@
+package oci
+
+import (
+	"github.com/atlaskerr/stori/schema"
+
+	"github.com/xeipuuv/gojsonschema"
+)
+
+var schemaFS = schema.FS(false)
+
+// ImageIndexLoader provides a JSONLoader that can be used to validate an image
+// index.
+var ImageIndexLoader = func() gojsonschema.JSONLoader {
+	path := "file:///oci/image-index.schema.json"
+	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
+}()
+
+// ImageManifestLoader provides a JSONLoader that can be used to validate an
+// image manifest.
+var ImageManifestLoader = func() gojsonschema.JSONLoader {
+	path := "file:///oci/image-manifest.schema.json"
+	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
+}()
+
+// ImageConfigLoader provides a JSONLoader that can be used to validate an image
+// config.
+var ImageConfigLoader = func() gojsonschema.JSONLoader {
+	path := "file:///oci/image-config.schema.json"
+	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
+}()
+
+// TagListLoader provides a JSONLoader that can be used to validate an tag list.
+var TagListLoader = func() gojsonschema.JSONLoader {
+	path := "file:///oci/tag-list.schema.json"
+	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
+}()
+
+// CatalogLoader provides a JSONLoader that can be used to validate a catalog of
+// repositories.
+var CatalogLoader = func() gojsonschema.JSONLoader {
+	path := "file:///oci/catalog.schema.json"
+	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
+}()
+
+// ErrorsLoader provides a JSONLoader that can be used to validate an
+// OCI-compliant error response.
+var ErrorsLoader = func() gojsonschema.JSONLoader {
+	path := "file:///oci/errors.schema.json"
+	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
+}()
