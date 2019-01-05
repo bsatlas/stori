7b29d097fe1af798db6ddd0cd03be3ef3a768354
Atlas Kerr <atlaskerr@gmail.com>
2019-01-04 20:48:00 -0600

    http: Add test for oci-verify handler.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

---
+++ http/verify_test.go
@@ -0,0 +1,50 @@
+// Copyright © 2018 Atlas Kerr atlaskerr@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package http
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/atlaskerr/stori/stori"
+)
+
+func TestVerifyHandler(t *testing.T) {
+	reg := stori.TestRegistry(t)
+
+	ln, addr := TestServer(t, reg)
+	defer ln.Close()
+
+	endpoint := addr + "/v2"
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		t.Fatalf("request failed: %v", err)
+	}
+
+	expectedCode := http.StatusOK
+	if resp.StatusCode != expectedCode {
+		t.Errorf("expected status code %v, got %v", expectedCode, resp.Status)
+	}
+
+	value, ok := resp.Header[VersionHeaderName]
+	if !ok {
+		t.Errorf("required header '%v' not found", VersionHeaderName)
+	}
+
+	if value[0] != VersionHeaderValue {
+		t.Errorf("invalid header value for '%v': expected '%v', got '%v'", VersionHeaderName, VersionHeaderValue, value[0])
+	}
+
+}
