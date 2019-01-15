18d07055f157e99af916a6998d6d1d155018e7ce
Atlas Kerr <atlaskerr@gmail.com>
2019-01-15 15:32:11 -0600

    scripts: Refactor build script.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- scripts/build-stori.sh
+++ scripts/build-stori.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-go build \
+#COMMIT= $(if $(git status --porcelain --untracked-files=no) then "${COMMIT_NO}-dirty" else "${COMMIT_NO}")
+VERSION=`cat ./VERSION`
+SHA=`git rev-parse HEAD 2> /dev/null || true`
+DIRTY=`git status --porcelain --untracked-files=no`
+
+if [ "$DIRTY" ]
+then
+	COMMIT="$SHA-dirty"
+else
+	COMMIT="$SHA"
+fi
+
+
+build() {
+	go build \
 	-ldflags "-X main.Version=$1 -X main.Commit=$2" \
 	-o bin/stori github.com/atlaskerr/stori/cmd/stori
+}
+
+build $VERSION $COMMIT
