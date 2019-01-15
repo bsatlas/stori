83f06d6bc9dae88e2edb8cbc0ba0a22cc9d633f4
Atlas Kerr <atlaskerr@gmail.com>
2019-01-14 20:48:35 -0600

    schema: Split embeded assets.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- schema/gen.go
+++
@@ -1,3 +0,0 @@
-package schema
-
-//go:generate esc -o=fs.go -modtime=1546544639 -pkg=schema -include=.*\.json$ -ignore=test-fixtures/* .
---
+++ schema/stori/gen.go
@@ -0,0 +1,3 @@
+package stori
+
+//go:generate esc -o=fs.go -modtime=1546544639 -pkg=stori -include=.*\.json$ -ignore=test-fixtures/* .
