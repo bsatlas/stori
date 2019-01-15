c0505a7c14b6101ac629d7e21d7cb2be9573c92d
Atlas Kerr <atlaskerr@gmail.com>
2019-01-15 12:39:55 -0600

    schema: Refactor loader functions.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- schema/loader.go
+++ schema/loader.go
@@ -1,14 +1,18 @@
 package schema
 
 import (
+	"fmt"
+
 	"github.com/xeipuuv/gojsonschema"
 )
 
 var schemaFS = FS(false)
 
-// ServerConfigLoader provides a JSONLoader that can be used to validate
-// a configuration file for serving a stori registry.
-var ServerConfigLoader = func() gojsonschema.JSONLoader {
-	path := "file:///server-config.schema.json"
+func load(file string) gojsonschema.JSONLoader {
+	path := fmt.Sprintf("file://%s", file)
 	return gojsonschema.NewReferenceLoaderFileSystem(path, schemaFS)
 }
+
+// ServerConfigLoader provides a JSONLoader that can be used to validate
+// a configuration file for serving a stori registry.
+var ServerConfigLoader = load("/server-config.schema.json")
