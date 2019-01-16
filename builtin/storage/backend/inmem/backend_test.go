b05a114021b96b6dc339dd3971c3207bded22f5b
Atlas Kerr <atlaskerr@gmail.com>
2019-01-16 12:58:20 -0600

    builtin:storage:backend: Add tests.
    This commit add test-cases for initializing and retrieving a jsonschema
    from the backend.
    
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- builtin/storage/backend/inmem/backend_test.go
+++ builtin/storage/backend/inmem/backend_test.go
@@ -1,1 +1,33 @@
 package inmem
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestNewBackend(t *testing.T) {
+	b, err := New()
+	if err != nil {
+		t.Fatal("could not initialize new backend")
+	}
+
+	if b.schema == nil {
+		t.Fatal("new backend doesn't contain a jsonschema")
+	}
+
+	if !json.Valid(b.schema) {
+		t.Fatal("jsonschema not valid json")
+	}
+
+	if b.db == nil {
+		t.Fatal("new backend doesn't have a database")
+	}
+}
+
+func TestGetSchema(t *testing.T) {
+	b, _ := New()
+	schema := b.GetSchema()
+	if schema == nil {
+		t.Fatal("no jsonschema returned")
+	}
+}
